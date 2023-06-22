6e278e268923b2710b3d1762d4819f7f055df8ec
fel <felicien.dehertogh@protonmail.com>
2023-06-22 23:42:32 +0200

    Add requirements.txt

--- leaders_scraper.py
+++ leaders_scraper.py
@@ -68,7 +68,3 @@ def get_leaders():
 def save(leaders_per_country):
     with open("leaders.json", "w", encoding="utf-8") as f:
         json.dump(leaders_per_country, f, ensure_ascii=False, indent=4)
-
-
-data = get_leaders()
-save(data)
